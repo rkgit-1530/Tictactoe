3fdcf83b5feccd8c9940b7270eb5c9c5371d6507
Raj Karan <160375104+rkgit-1530@users.noreply.github.com>
2026-04-26 12:06:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let gameActive = true;
 let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
-const winningMessage = () => `Player ${currentPlayer} has won!`;
+const winningMessage = () => `Player O has won!`;
 const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
